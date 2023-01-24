7268271db0d28f5f590298329143c00c749c508c
ankiwoong <ankiwoong@gmail.com>
2023-01-24 15:58:52 +0900

    예제 4

--- ex4.sh
+++ ex4.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-OS_TYPE="redhat"
-
-# OS_TYPE에 값이 있으므로 redhat 출력
-echo ${OS_TYPE:?null or not set}
-echo ${OS_TYPE?not set}
-
-# 변수 OS_TYPE에 null을 저장
 OS_TYPE=""
 
-# ${변수:?에러 메시지}을 사용하면 null 은 값으로 취급하지 않으므로 에러 메시지 출력
-echo ${OS_TYPE:?null or not set}
+# OS_TYPE의 값이 null이기 떄문에 초기화가 되지 않았다고 판단하고 null 출력
+echo ${OS_TYPE:+redhat}
 
-# 비정상 종료이므로 특수 매개변수 $?은 1 을 출력
-echo $?
+# OS_TYPE이 null 로 초기화되었다고 판단하고 redhat 출력
+echo ${OS_TYPE+redhat}
+
+# OS_TYPE을 삭제하면 변수가 존재하지 않으므로 null 출력
+unset OS_TYPE
+echo ${OS_TYPE:+redhat}
+echo ${OS_TYPE+redhat}
